c07d14f01fcc1c7aa5031b5b8b0afca9a63cbeb4
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2022-12-22 20:58:27 +0100

    Simplify Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,22 +6,6 @@ LOCAL_SRC_FILES := $(wildcard ../src/*.cpp)
 LOCAL_CFLAGS += -DNDEBUG -O3 -flto -fexperimental-new-pass-manager -Wfatal-errors -Wall -Wextra -Wshadow
 LOCAL_CPPFLAGS += 
 LOCAL_CPP_FEATURES := 
-
-ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
-  LOCAL_CFLAGS += 
-else
-  ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-    LOCAL_CFLAGS += 
-  else
-    ifeq ($(TARGET_ARCH_ABI), x86)
-      LOCAL_CFLAGS += -mssse3 -mfpmath=sse -m32
-    else
-      ifeq ($(TARGET_ARCH_ABI), x86_64)
-        LOCAL_CFLAGS += -msse4.2 -mpopcnt -m64
-      endif
-    endif
-  endif
-endif
-
+LOCAL_CFLAGS += 
 LOCAL_LDFLAGS += 
 include $(BUILD_EXECUTABLE)

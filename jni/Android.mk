f03e32e8608ce8b1132655da943a64a3ebb7716a
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2022-12-22 19:30:43 +0100

    /jni for TogaII

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,18 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := Stockfish
+LOCAL_MODULE := Fruit
 LOCAL_C_INCLUDES := 
-LOCAL_SRC_FILES := $(wildcard ../src/*)
-LOCAL_CFLAGS += -std=c++17 -O3 -flto -fexperimental-new-pass-manager -Wfatal-errors -Wall -Wextra -Wshadow -DNNUE_EMBEDDING_OFF -DUSE_PTHREADS
+LOCAL_SRC_FILES := $(wildcard ../src/*.cpp)
+LOCAL_CFLAGS += -DNDEBUG -O3 -flto -fexperimental-new-pass-manager -Wfatal-errors -Wall -Wextra -Wshadow
 LOCAL_CPPFLAGS += 
 LOCAL_CPP_FEATURES := 
 
 ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
-  LOCAL_CFLAGS += -DIS_64BIT -DUSE_POPCNT
-endif
-ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-  LOCAL_CFLAGS += -DUSE_POPCNT
+  LOCAL_CFLAGS += 
+else
+  ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
+    LOCAL_CFLAGS += 
+  else
+    ifeq ($(TARGET_ARCH_ABI), x86)
+      LOCAL_CFLAGS += -mssse3 -mfpmath=sse -m32
+    else
+      ifeq ($(TARGET_ARCH_ABI), x86_64)
+        LOCAL_CFLAGS += -msse4.2 -mpopcnt -m64
+      endif
+    endif
+  endif
 endif
 
-LOCAL_LDFLAGS +=
+LOCAL_LDFLAGS += 
 include $(BUILD_EXECUTABLE)

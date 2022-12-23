728e873f60083130b5064a38db9a2831298bd73b
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2022-12-23 03:36:11 +0100

    Silence warnings

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Fruit
 LOCAL_C_INCLUDES := 
 LOCAL_SRC_FILES := $(wildcard ../src/*.cpp)
-LOCAL_CFLAGS += -DNDEBUG -O3 -flto -fexperimental-new-pass-manager -Wfatal-errors -Wall -Wextra -Wshadow
+LOCAL_CFLAGS += -DNDEBUG -O3 -flto -fexperimental-new-pass-manager -Wfatal-errors
 LOCAL_CPPFLAGS += 
 LOCAL_CPP_FEATURES := 
 LOCAL_CFLAGS += 

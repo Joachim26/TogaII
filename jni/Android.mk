40b306a2f46d8c887770d3a4372ac3711d4f8221
Joachim26 <69502420+Joachim26@users.noreply.github.com>
2025-04-19 13:03:52 +0200

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Fruit
 LOCAL_C_INCLUDES := 
 LOCAL_SRC_FILES := $(wildcard ../src/*.cpp)
-LOCAL_CFLAGS += -DNDEBUG -O3 -flto -fexperimental-new-pass-manager -Wfatal-errors
+LOCAL_CFLAGS += -DNDEBUG -O3 -flto -Wfatal-errors
 LOCAL_CPPFLAGS += 
 LOCAL_CPP_FEATURES := 
 LOCAL_CFLAGS += 
